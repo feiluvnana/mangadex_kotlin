fc2946d4d7800f2d7882617444d986c011b5b27f
feiluvnana <ntd271102@gmail.com>
2024-10-15 02:22:03 +0700

    add more ui

--- app/src/main/java/com/fln/mangadex/MainActivity.kt
+++ app/src/main/java/com/fln/mangadex/MainActivity.kt
@@ -3,55 +3,70 @@ package com.fln.mangadex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Book
-import androidx.compose.material.icons.rounded.Update
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.fln.mangadex.pages.LibraryPage
-import com.fln.mangadex.pages.UpdatesPage
+import androidx.navigation.compose.*
+import com.fln.mangadex.pages.*
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
+    Thread.setDefaultUncaughtExceptionHandler(
+      object : Thread.UncaughtExceptionHandler {
+        override fun uncaughtException(p0: Thread, p1: Throwable) {}
+      })
+
     setContent {
       val navController = rememberNavController()
 
-      Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-          val route =
-            navController
-              .currentBackStackEntryAsState()
-              .value
-              ?.destination
-              ?.route
+      MaterialTheme(colorScheme = darkColorScheme()) {
+        Scaffold(
+          modifier = Modifier.fillMaxSize(),
+          bottomBar = {
+            val route =
+              navController
+                .currentBackStackEntryAsState()
+                .value
+                ?.destination
+                ?.route
 
-          NavigationBar {
-            NavigationBarItem(
-              selected = route == "library",
-              onClick = { navController.navigate("library") },
-              icon = { Icon(Icons.Rounded.Book, null) },
-              label = { Text("Library") })
-            NavigationBarItem(
-              selected = route == "updates",
-              onClick = { navController.navigate("updates") },
-              icon = { Icon(Icons.Rounded.Update, null) },
-              label = { Text("Updates") })
+            NavigationBar {
+              NavigationBarItem(
+                selected = route == "library",
+                onClick = { navController.navigate("library") },
+                icon = { Icon(Icons.Rounded.Book, null) },
+                label = { Text("Library") })
+              NavigationBarItem(
+                selected = route == "updates",
+                onClick = { navController.navigate("updates") },
+                icon = { Icon(Icons.Rounded.Update, null) },
+                label = { Text("Updates") })
+              NavigationBarItem(
+                selected = route == "history",
+                onClick = { navController.navigate("history") },
+                icon = { Icon(Icons.Rounded.History, null) },
+                label = { Text("History") })
+              NavigationBarItem(
+                selected = route == "more",
+                onClick = { navController.navigate("more") },
+                icon = { Icon(Icons.Rounded.MoreHoriz, null) },
+                label = { Text("More") })
+            }
+          }) {
+            NavHost(
+              navController = navController,
+              startDestination = "library",
+              modifier = Modifier.padding(it)) {
+                composable("library") { LibraryPage() }
+                composable("updates") { UpdatesPage() }
+                composable("history") { HistoryPage() }
+                composable("more") { MorePage() }
+              }
           }
-        }) {
-          NavHost(navController, startDestination = "library") {
-            composable("library") { LibraryPage() }
-            composable("updates") { UpdatesPage() }
-          }
-        }
+      }
     }
   }
 }
--- app/src/main/java/com/fln/mangadex/pages/MorePage.kt
+++ app/src/main/java/com/fln/mangadex/pages/MorePage.kt
@@ -1,10 +1,104 @@
 package com.fln.mangadex.pages
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.Help
+import androidx.compose.material.icons.automirrored.rounded.Label
+import androidx.compose.material.icons.rounded.CloudOff
+import androidx.compose.material.icons.rounded.Download
+import androidx.compose.material.icons.rounded.HistoryToggleOff
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.QueryStats
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.rounded.SettingsBackupRestore
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.*
+import com.fln.mangadex.R
 
 @Composable
 fun MorePage() {
-  Box { Text("More Page") }
+  Scaffold {
+    MaterialTheme.colorScheme
+    val screenSize: Pair<Dp, Dp> =
+      Pair(
+        LocalConfiguration.current.screenWidthDp.dp,
+        LocalConfiguration.current.screenHeightDp.dp)
+    LocalConfiguration.current.densityDpi
+
+    LazyColumn {
+      item {
+        Box(modifier = Modifier.fillMaxWidth().height(screenSize.second / 4)) {
+          Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center))
+        }
+      }
+      item { HorizontalDivider() }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.CloudOff, null) },
+          headlineContent = { Text("Downloaded only") },
+          supportingContent = { Text("Filter all entries in your library") },
+          trailingContent = { Switch(checked = false, onCheckedChange = {}) })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.HistoryToggleOff, null) },
+          headlineContent = { Text("Incognito mode") },
+          supportingContent = { Text("Pauses reading history") },
+          trailingContent = { Switch(checked = false, onCheckedChange = {}) })
+      }
+      item { HorizontalDivider() }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.Download, null) },
+          headlineContent = { Text("Download queue") })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.AutoMirrored.Rounded.Label, null) },
+          headlineContent = { Text("Categories") })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.QueryStats, null) },
+          headlineContent = { Text("Statistics") })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.SettingsBackupRestore, null) },
+          headlineContent = { Text("Backup and restore") })
+      }
+      item { HorizontalDivider() }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.Settings, null) },
+          headlineContent = { Text("Settings") })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.Rounded.Info, null) },
+          headlineContent = { Text("About") })
+      }
+      item {
+        ListItem(
+          leadingContent = { Icon(Icons.AutoMirrored.Rounded.Help, null) },
+          headlineContent = { Text("Help") })
+      }
+    }
+  }
 }
