e13955bb456c3b8f1271769f2cfbda73a6a0237e
feiluvnana <ntd271102@gmail.com>
2024-10-29 17:31:15 +0700

    separate logic biometric

--- app/src/main/java/com/fln/mangadex/core/repositories/auth/BiometricRepository.kt
+++ app/src/main/java/com/fln/mangadex/core/repositories/auth/BiometricRepository.kt
@@ -3,7 +3,6 @@ package com.fln.mangadex.core.repositories.auth
 import android.app.Activity
 import android.app.KeyguardManager
 import android.content.Context
-import android.os.Build.VERSION
 import android.widget.Toast
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
@@ -14,7 +13,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.flow.first
 import javax.inject.Singleton
 
 @Module
@@ -28,76 +27,91 @@ object BiometricModule {
 }
 
 class BiometricRepository {
+  companion object {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private val MainActivity.km: KeyguardManager
+      get() = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+    private val MainActivity.bm: BiometricManager
+      get() = BiometricManager.from(this)
+  }
+
   fun authenticate(
     activity: MainActivity,
     onSuccess: () -> Unit
   ) {
-    if (VERSION.SDK_INT <= 29) authenticateForApi29AndLower(activity, onSuccess)
-  }
-
-  private fun authenticateForApi29AndLower(
-    activity: MainActivity,
-    onSuccess: () -> Unit
-  ) {
     try {
-      val coroutineScope = CoroutineScope(Dispatchers.Main)
-      val executor = ContextCompat.getMainExecutor(activity)
-      val biometricManager = BiometricManager.from(activity)
       val canAuthenticateByBiometric =
-        biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
-      val keyguardManager =
-        activity.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-      val canAuthenticateByKeyguard = keyguardManager.isDeviceSecure
-      when (canAuthenticateByBiometric) {
-        BiometricManager.BIOMETRIC_SUCCESS -> {
-          val callback = object : BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(
-              errorCode: Int,
-              errString: CharSequence
-            ) {
-              if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON && canAuthenticateByKeyguard) {
-                val intent =
-                  keyguardManager.createConfirmDeviceCredentialIntent(
-                    "Require unlock",
-                    "Please enter your password"
-                  )
-                activity.startActivityForResultLauncher.launch(intent)
-                val flow = activity.startActivityResultFlow.takeWhile { false }
-                coroutineScope.launch {
-                  flow.collect {
-                    if (it?.resultCode == Activity.RESULT_OK) onSuccess()
-                  }
-                }
-              }
-              Toast.makeText(activity, errString, Toast.LENGTH_SHORT).show()
-            }
+        activity.bm.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
+      val canAuthenticateByKeyguard = activity.km.isDeviceSecure
 
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-              onSuccess()
-            }
-
-            override fun onAuthenticationFailed() {
-              Toast.makeText(activity, "Failed", Toast.LENGTH_SHORT).show()
-            }
-          }
-
-          val biometricPrompt = BiometricPrompt(activity, executor, callback)
-          val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Require unlock")
-            .setSubtitle("Authenticate to continue")
-            .setNegativeButtonText(if (canAuthenticateByKeyguard) "Use password instead" else "Cancel")
-            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
-            .build()
-          biometricPrompt.authenticate(promptInfo)
-        }
-
-        else -> {
-          Toast.makeText(activity, "Cannot use biometrics", Toast.LENGTH_SHORT)
-            .show()
-        }
+      if (!canAuthenticateByBiometric && !canAuthenticateByKeyguard) {
+        Toast.makeText(
+          activity,
+          "You must have Biometric or Device Credential to use this function.",
+          Toast.LENGTH_SHORT
+        )
+          .show()
+        return
       }
+
+      if (!canAuthenticateByBiometric) {
+        authenticateByKeyguardManager(activity, onSuccess)
+        return
+      }
+      
+      authenticateByBiometricPrompt(activity, onSuccess)
     } catch (e: Exception) {
       Toast.makeText(activity, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
     }
+  }
+
+  private fun authenticateByBiometricPrompt(
+    activity: MainActivity,
+    onSuccess: () -> Unit
+  ) {
+    val canAuthenticateByKeyguard = activity.km.isDeviceSecure
+    val executor = ContextCompat.getMainExecutor(activity)
+    val callback = object : BiometricPrompt.AuthenticationCallback() {
+      override fun onAuthenticationError(
+        errorCode: Int,
+        errString: CharSequence
+      ) {
+        if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON && canAuthenticateByKeyguard) {
+          authenticateByKeyguardManager(activity, onSuccess)
+          return
+        }
+      }
+
+      override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+        onSuccess()
+      }
+    }
+    val biometricPrompt = BiometricPrompt(activity, executor, callback)
+    val promptInfo = BiometricPrompt.PromptInfo.Builder()
+      .setTitle("Require unlock")
+      .setSubtitle("Authenticate to continue")
+      .setNegativeButtonText(if (canAuthenticateByKeyguard) "Use password instead" else "Cancel")
+      .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+      .build()
+    biometricPrompt.authenticate(promptInfo)
+  }
+
+  private fun authenticateByKeyguardManager(
+    activity: MainActivity,
+    onSuccess: () -> Unit
+  ) {
+    val intent =
+      activity.km.createConfirmDeviceCredentialIntent(
+        "Require unlock",
+        "Please enter your password"
+      )
+    coroutineScope.launch {
+      val result = activity.startActivityResultFlow.first()
+      if (result?.resultCode == Activity.RESULT_OK) {
+        onSuccess()
+      }
+    }
+    activity.startActivityForResultLauncher.launch(intent)
+
   }
 }
